e35f3dc4a431452367414512cb7f352e0af3dae6
Adam Goldwater <dmgldwtr@gmail.com>
2019-12-10 14:44:57 -0600

    updates time_logs and sprints controllers

--- app/controllers/sprints_controller.rb
+++ app/controllers/sprints_controller.rb
@@ -1,2 +1,42 @@
 class SprintsController < ApplicationController
+  def index
+    @sprints = Sprint.all
+  end
+
+  def new
+    @sprint = Sprint.new
+  end
+
+  def create
+    Sprint.update_all({
+      current: false
+    })
+
+    Sprint.create({
+      sprint: params[:sprint]
+      current: true
+    })
+  end
+
+  def show
+    @sprint = Sprint.find(params[:id])
+  end
+
+  def edit
+    @sprint = Sprint.find(params[:id])
+  end
+
+  def update
+    sprint = Sprint.find(params[:id])
+
+    Sprint.assign_attributes({
+      sprint: params[:sprint]
+      current: true
+    })
+  end
+
+  def destroy
+    sprint = Sprint.find(params[:id])
+    sprint.destroy
+  end
 end
