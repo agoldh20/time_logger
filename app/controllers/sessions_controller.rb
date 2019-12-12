e1ced932653191dff6bf55159112073e1b3d2c77
Adam Goldwater <dmgldwtr@gmail.com>
2019-12-12 16:31:14 -0600

    WIP sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Successfully Logged In"
+      redirect_to '/'
+    else
+      flash[:warning] = "Invalid Something or Other"
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Logged Out"
+    redirect_to '/login'
+  end
 end
